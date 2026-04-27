522ce739242daf1bd7fc17be3b48f5c208415fb6
Pramit Ranjan <pramitbit@gmail.com>
2026-04-27 02:51:56 +0000

    Commit message: Add React frontend for Octofit Tracker

---
+++ octofit-tracker/frontend/src/components/Activities.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+
+const Activities = () => {
+  const [activities, setActivities] = useState([]);
+  const codespace = process.env.REACT_APP_CODESPACE_NAME;
+  const endpoint = codespace
+    ? `https://${codespace}-8000.app.github.dev/api/activities/`
+    : '/api/activities/';
+
+  useEffect(() => {
+    fetch(endpoint)
+      .then(res => res.json())
+      .then(data => {
+        const results = data.results || data;
+        setActivities(results);
+        console.log('Activities endpoint:', endpoint);
+        console.log('Fetched activities:', results);
+      });
+  }, [endpoint]);
+
+  return (
+    <div className="container mt-4">
+      <h2>Activities</h2>
+      <ul className="list-group">
+        {activities.map((activity, idx) => (
+          <li className="list-group-item" key={idx}>
+            {activity.user} - {activity.activity} ({activity.duration} min)
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Activities;
